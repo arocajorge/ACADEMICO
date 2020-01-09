723cd4fe08dfd14b99f5f0867892b9a41c0fd0e8
Jorge Aroca <jaroca@fixed-sa.com>
2020-01-09 17:40:35 -0500

    1

---
+++ Academico/Core.Web/Areas/Academico/Controllers/ParametrizacionPlantillaController.cs
@@ -0,0 +1,30 @@
+﻿using Core.Info.Helps;
+using Core.Web.Helps;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Core.Web.Areas.Academico.Controllers
+{
+    public class ParametrizacionPlantillaController : Controller
+    {
+        #region Index
+        public ActionResult Index()
+        {
+            cl_filtros_Info model = new cl_filtros_Info
+            {
+                IdEmpresa = Convert.ToInt32(SessionFixed.IdEmpresa),
+                IdSucursal = Convert.ToInt32(SessionFixed.IdSucursal)
+            };
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Index(cl_filtros_Info model)
+        {
+            return View(model);
+        }
+        #endregion
+    }
+}
