59a6af92491f55c9004becdb7c826680eeac4fa2
Jorge Aroca <jaroca@fixed-sa.com>
2020-01-09 17:56:34 -0500

    aa

--- Academico/Core.Bus/Contabilidad/ct_anio_fiscal_x_tb_sucursal_Bus.cs
+++ Academico/Core.Bus/Contabilidad/ct_anio_fiscal_x_tb_sucursal_Bus.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Core.Erp.Bus.Contabilidad
+namespace Core.Bus.Contabilidad
 {
     public class ct_anio_fiscal_x_tb_sucursal_Bus
     {
--- Academico/Core.Web/Areas/Academico/Controllers/ParametrizacionPlantillaController.cs
+++ Academico/Core.Web/Areas/Academico/Controllers/ParametrizacionPlantillaController.cs
@@ -1,10 +1,13 @@
-﻿using Core.Info.Helps;
+﻿using Core.Bus.Contabilidad;
+using Core.Info.Helps;
 using Core.Web.Helps;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Core.Info.Academico;
+using Core.Bus.Academico;
 
 namespace Core.Web.Areas.Academico.Controllers
 {
@@ -24,6 +27,15 @@ namespace Core.Web.Areas.Academico.Controllers
         public ActionResult Index(cl_filtros_Info model)
         {
             return View(model);
+        }
+        #endregion
+
+        #region Combos bajo demanda
+        ct_plancta_Bus bus_plancta = new ct_plancta_Bus();
+        public ActionResult CmbCuentaDebe()
+        {
+            aca_AnioLectivo_Curso_Plantilla_Parametrizacion_Info model = new aca_AnioLectivo_Curso_Plantilla_Parametrizacion_Info();
+            return PartialView("_CmbCuentaDebe", model);
         }
         #endregion
     }
