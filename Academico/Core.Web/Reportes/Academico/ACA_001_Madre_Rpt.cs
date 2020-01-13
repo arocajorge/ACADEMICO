518da49396ef8992a08336b7cc649e507b2bdbfe
WendyCherres <wendycherres@gmail.com>
2020-01-13 16:57:46 -0500

    ..

---
+++ Academico/Core.Web/Reportes/Academico/ACA_001_Madre_Rpt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+
+namespace Core.Web.Reportes.Academico
+{
+    public partial class ACA_001_Madre_Rpt : DevExpress.XtraReports.UI.XtraReport
+    {
+        public ACA_001_Madre_Rpt()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
