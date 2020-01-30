4cdc7b750baf09cb7c9a2b19742849df6bcb9939
arocajorge <arocajorge92@gmail.com>
2020-01-30 12:34:39 -0500

    .

--- Academico/Core.Data/Base/ModelFacturacion.Context.cs
+++ Academico/Core.Data/Base/ModelFacturacion.Context.cs
@@ -60,5 +60,6 @@ namespace Core.Data.Base
         public virtual DbSet<fa_factura_det> fa_factura_det { get; set; }
         public virtual DbSet<fa_factura_resumen> fa_factura_resumen { get; set; }
         public virtual DbSet<fa_PuntoVta_x_seg_usuario> fa_PuntoVta_x_seg_usuario { get; set; }
+        public virtual DbSet<vwfa_factura_ParaContabilizarAcademico> vwfa_factura_ParaContabilizarAcademico { get; set; }
     }
 }
