33938f611c154533d242ccca64de86ea0a1fd1fb
arocajorge <arocajorge92@gmail.com>
2020-02-03 11:27:20 -0500

    .

---
+++ Academico/Core.Bus/CuentasPorCobrar/cxc_ConciliacionNotaCreditoDet_Bus.cs
@@ -0,0 +1,38 @@
+﻿using Core.Data.CuentasPorCobrar;
+using Core.Info.CuentasPorCobrar;
+using System;
+using System.Collections.Generic;
+
+namespace Core.Bus.CuentasPorCobrar
+{
+    public class cxc_ConciliacionNotaCreditoDet_Bus
+    {
+        cxc_ConciliacionNotaCreditoDet_Data odata = new cxc_ConciliacionNotaCreditoDet_Data();
+
+        public List<cxc_ConciliacionNotaCreditoDet_Info> GetList(int IdEmpresa, decimal IdConciliacion)
+        {
+            try
+            {
+                return odata.GetList(IdEmpresa, IdConciliacion);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public List<cxc_ConciliacionNotaCreditoDet_Info> GetListPorCruzar(int IdEmpresa, decimal IdAlumno)
+        {
+            try
+            {
+                return odata.GetListPorCruzar(IdEmpresa, IdAlumno);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ Academico/Core.Web/Areas/CuentasPorCobrar/Controllers/ConciliacionNotaCreditoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Core.Info.General;
+using Core.Bus.CuentasPorCobrar;
+using Core.Info.CuentasPorCobrar;
+
+namespace Core.Web.Areas.CuentasPorCobrar.Controllers
+{
+    public class ConciliacionNotaCreditoController : Controller
+    {
+        // GET: CuentasPorCobrar/ConciliacionNotaCredito
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
