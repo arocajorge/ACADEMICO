f447736d6a4eb4d0b5cc51936cd2f1c3b9713d5d
arocajorge <arocajorge92@gmail.com>
2020-02-03 11:52:57 -0500

    1

--- Academico/Core.Web/Areas/CuentasPorCobrar/Controllers/ConciliacionNotaCreditoController.cs
+++ Academico/Core.Web/Areas/CuentasPorCobrar/Controllers/ConciliacionNotaCreditoController.cs
@@ -6,21 +6,74 @@ using System.Web.Mvc;
 using Core.Info.General;
 using Core.Bus.CuentasPorCobrar;
 using Core.Info.CuentasPorCobrar;
+using Core.Info.Helps;
+using Core.Web.Helps;
 
 namespace Core.Web.Areas.CuentasPorCobrar.Controllers
 {
     public class ConciliacionNotaCreditoController : Controller
     {
-        // GET: CuentasPorCobrar/ConciliacionNotaCredito
-        //public ActionResult Index()
-        //{
-        //    return View();
-        //}
+        #region Variables
+        cxc_ConciliacionNotaCredito_List Lista = new cxc_ConciliacionNotaCredito_List();
+        cxc_ConciliacionNotaCredito_Bus busConciliacion = new cxc_ConciliacionNotaCredito_Bus();
+        #endregion
 
-        //[HttpPost]
-        //public ActionResult Index()
-        //{
-        //    return View();
-        //}
+        #region Index
+        public ActionResult Index()
+        {
+            #region Validar Session
+            if (string.IsNullOrEmpty(SessionFixed.IdTransaccionSession))
+                return RedirectToAction("Login", new { Area = "", Controller = "Account" });
+            SessionFixed.IdTransaccionSession = (Convert.ToDecimal(SessionFixed.IdTransaccionSession) + 1).ToString();
+            SessionFixed.IdTransaccionSessionActual = SessionFixed.IdTransaccionSession;
+            #endregion
+
+            cl_filtros_Info model = new cl_filtros_Info
+            {
+                IdEmpresa = Convert.ToInt32(SessionFixed.IdEmpresa),
+                IdTransaccionSession = Convert.ToDecimal(SessionFixed.IdTransaccionSessionActual)
+            };
+
+            Lista.set_list(busConciliacion.GetList(model.IdEmpresa, model.fecha_ini, model.fecha_fin), model.IdTransaccionSession);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(cl_filtros_Info model)
+        {
+            SessionFixed.IdTransaccionSessionActual = model.IdTransaccionSession.ToString();
+            Lista.set_list(busConciliacion.GetList(model.IdEmpresa, model.fecha_ini, model.fecha_fin), model.IdTransaccionSession);
+            return View(model);
+        }
+
+        [ValidateInput(false)]
+        public ActionResult GridViewPartial_ConciliacionNC()
+        {
+            SessionFixed.IdTransaccionSessionActual = Request.Params["TransaccionFixed"] != null ? Request.Params["TransaccionFixed"].ToString() : SessionFixed.IdTransaccionSessionActual;
+            List<cxc_ConciliacionNotaCredito_Info> model = Lista.get_list(Convert.ToDecimal(SessionFixed.IdTransaccionSessionActual));
+            return PartialView("_GridViewPartial_ConciliacionNC", model);
+        }
+        #endregion
+
+    }
+
+    public class cxc_ConciliacionNotaCredito_List
+    {
+        string Variable = "aca_AnioLectivoCalificacionHistorico_Info";
+        public List<cxc_ConciliacionNotaCredito_Info> get_list(decimal IdTransaccionSession)
+        {
+            if (HttpContext.Current.Session[Variable + IdTransaccionSession.ToString()] == null)
+            {
+                List<cxc_ConciliacionNotaCredito_Info> list = new List<cxc_ConciliacionNotaCredito_Info>();
+
+                HttpContext.Current.Session[Variable + IdTransaccionSession.ToString()] = list;
+            }
+            return (List<cxc_ConciliacionNotaCredito_Info>)HttpContext.Current.Session[Variable + IdTransaccionSession.ToString()];
+        }
+
+        public void set_list(List<cxc_ConciliacionNotaCredito_Info> list, decimal IdTransaccionSession)
+        {
+            HttpContext.Current.Session[Variable + IdTransaccionSession.ToString()] = list;
+        }
     }
 }
