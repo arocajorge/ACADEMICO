d4add9666caf9c318596ddba7eb1300de451f624
EdgarSebastian01 <60885441+EdgarSebastian01@users.noreply.github.com>
2020-02-10 13:12:43 -0600

    .

--- Academico/Core.Data/Reportes/CuentasPorCobrar/CXC_004_Data.cs
+++ Academico/Core.Data/Reportes/CuentasPorCobrar/CXC_004_Data.cs
@@ -1,9 +1,8 @@
-﻿using Core.Info.Reportes.CuentasPorCobrar;
+﻿using Core.Data.Base;
+using Core.Info.Reportes.CuentasPorCobrar;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Core.Data.Reportes.CuentasPorCobrar
 {
@@ -13,14 +12,37 @@ namespace Core.Data.Reportes.CuentasPorCobrar
         {
             try
             {
-                 
+                FechaCorte = FechaCorte.Date;
 
-                List < CXC_004_Info > Lista = new List<CXC_004_Info>();
+                List <CXC_004_Info> Lista = new List<CXC_004_Info>();
+
+                using (EntitiesReportes db = new EntitiesReportes())
+                {
+                    var lst = db.SPCXC_004(IdEmpresa, IdUsuario, FechaCorte).ToList();
+
+                    foreach (var item in lst)
+                    {
+                        Lista.Add(new CXC_004_Info
+                        {
+                            IdEmpresa = item.IdEmpresa,
+                            IdAlumno = item.IdAlumno,
+                            IdAnio = item.IdAnio,
+                            IdUsuario = item.IdUsuario,
+                            NomAnio = item.NomAnio,
+                            CodigoAlumno = item.CodigoAlumno,
+                            NombreAlumno = item.NombreAlumno,
+                            IdJornada = item.IdJornada,
+                            NombreJornada = item.NombreJornada,
+                            SaldoDeudor = item.SaldoDeudor,
+                            SaldoAcreedor = item.SaldoAcreedor,
+                            SaldoFinal = item.SaldoFinal,
+
+                        });
+                    }
+                }
 
 
-
-
-                return Lista;
+                    return Lista;
             }
             catch (Exception)
             {
