4ff13ea95684d4a81f85fb321e2ecefadc5bc32f
EdgarSebastian01 <60885441+EdgarSebastian01@users.noreply.github.com>
2020-02-13 14:09:04 -0500

    ...

---
+++ Academico/Core.Bus/Reportes/Academico/ACA_006_ Bus.cs
@@ -0,0 +1,24 @@
+﻿using Core.Data.Reportes.Academico;
+using Core.Info.Reportes.Academico;
+using System;
+using System.Collections.Generic;
+
+namespace Core.Bus.Reportes.Academico
+{
+    public class ACA_006__Bus
+    {
+        ACA_006_Data odata = new ACA_006_Data();
+        public List<ACA_006_Info> Getlist(int IdEmpresa, int IdAnio, int IdSede, int IdNivel, int IdJornada, int IdCurso)
+        {
+            try
+            {
+                return odata.Getlist(IdEmpresa, IdAnio, IdSede, IdNivel, IdJornada, IdCurso);
+            }
+
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
