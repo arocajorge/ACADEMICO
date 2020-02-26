a730e78d9453be0411cc2704a463010f59c8c5e1
EdgarSebastian01 <60885441+EdgarSebastian01@users.noreply.github.com>
2020-02-26 14:48:57 -0500

    ...

---
+++ Academico/Core.Web/Areas/Reportes/Controllers/CajaReportesController.cs
@@ -0,0 +1,44 @@
+﻿using Core.Bus.General;
+using Core.Bus.Reportes.Caja;
+using Core.Web.Helps;
+using Core.Web.Reportes.Caja;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Core.Web.Areas.Reportes.Controllers
+{
+    public class CajaReportesController : Controller
+    {
+        
+        tb_sis_reporte_x_tb_empresa_Bus bus_rep_x_emp = new tb_sis_reporte_x_tb_empresa_Bus();
+        string RootReporte = System.IO.Path.GetTempPath() + "Rpt_Facturacion.repx";
+        // GET: Reportes/CajaReportes
+        public ActionResult CAJ_001( int IdTipoCbte=0, int IdCbteCble = 0)
+        {
+            CAJ_001_Rpt model = new CAJ_001_Rpt();
+            CAJ_001_Bus bus_rpt = new CAJ_001_Bus();
+
+            #region Cargo diseño desde base
+            int IdEmpresa = Convert.ToInt32(SessionFixed.IdEmpresa);
+            var reporte = bus_rep_x_emp.GetInfo(IdEmpresa, "CAJ_001");
+            if (reporte != null)
+            {
+                System.IO.File.WriteAllBytes(RootReporte, reporte.ReporteDisenio);
+                model.LoadLayout(RootReporte);
+            }
+            #endregion
+            model.p_IdEmpresa.Value = Convert.ToInt32(SessionFixed.IdEmpresa);
+            model.p_IdTipoCbte.Value = IdTipoCbte;
+            model.p_IdCbteCble.Value = IdCbteCble;
+            //model.p_Fecha.Value = cm_fecha;
+            //model.p_IdCaja.Value = IdCaja;
+            model.usuario = SessionFixed.IdUsuario;
+            model.empresa = SessionFixed.NomEmpresa;
+            model.RequestParameters = false;
+            return View(model);
+        }
+    }
+}
---
+++ Academico/Core.Web/Reportes/Caja/CAJ_001_Rpt.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+using Core.Bus.Reportes.Caja;
+using Core.Info.Reportes.Caja;
+using System.Collections.Generic;
+
+namespace Core.Web.Reportes.Caja
+{
+    public partial class CAJ_001_Rpt : DevExpress.XtraReports.UI.XtraReport
+    {
+        CAJ_001_Bus bus_rpt = new CAJ_001_Bus();
+        public string usuario { get; set; }
+        public string empresa { get; set; }
+        public CAJ_001_Rpt()
+        {
+            InitializeComponent();
+        }
+
+        private void CAJ_001_Rpt_BeforePrint(object sender, System.Drawing.Printing.PrintEventArgs e)
+        {
+            lbl_fecha.Text = DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss");
+            lbl_empresa.Text = empresa;
+            lbl_usuario.Text = usuario;
+            int IdEmpresa = string.IsNullOrEmpty(p_IdEmpresa.Value.ToString()) ? 0 : Convert.ToInt32(p_IdEmpresa.Value);
+            int IdCaja = string.IsNullOrEmpty(p_IdCaja.Value.ToString()) ? 0:  Convert.ToInt32(p_IdCaja.Value);
+            DateTime cm_fecha = string.IsNullOrEmpty(p_Fecha.Value.ToString()) ? DateTime.Now : Convert.ToDateTime(p_Fecha.Value);
+            List<CAJ_001_Info> Lista = bus_rpt.GetList(IdEmpresa, IdCaja, cm_fecha);
+
+
+        }
+    }
+}
