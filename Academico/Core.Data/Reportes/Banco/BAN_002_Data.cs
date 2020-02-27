6744c3b47d5022fd80ae93da8637e9ba3ba8af69
EdgarSebastian01 <60885441+EdgarSebastian01@users.noreply.github.com>
2020-02-27 12:31:51 -0500

    ....

---
+++ Academico/Core.Data/Reportes/Banco/BAN_002_Data.cs
@@ -0,0 +1,56 @@
+﻿using Core.Data.Base;
+using Core.Info.Reportes.Banco;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Data.Reportes.Banco
+{
+    public class BAN_002_Data
+    {
+        public List<BAN_002_Info> get_list(int IdEmpresa, int IdTipocbte, decimal IdCbteCble)
+        {
+            try
+            {
+                List<BAN_002_Info> Lista;
+                using (EntitiesReportes Context = new EntitiesReportes())
+                {
+                    Lista = (from q in Context.VWBAN_002
+                             where q.IdEmpresa == IdEmpresa
+                             && q.IdTipocbte == IdTipocbte
+                             && q.IdCbteCble == IdCbteCble
+                             select new BAN_002_Info
+                             {
+                                 CodTipoCbteBan = q.CodTipoCbteBan,
+                                 IdEmpresa = q.IdEmpresa,
+                                 IdCbteCble = q.IdCbteCble,
+                                 IdTipocbte = q.IdTipocbte,
+                                 IdBanco = q.IdBanco,
+                                 ba_descripcion = q.ba_descripcion,
+                                 cb_Fecha = q.cb_Fecha,
+                                 cb_Observacion = q.cb_Observacion,
+                                 Estado = q.Estado,
+                                 IdTipoNota = q.IdTipoNota,
+                                 Descripcion_TipoNota = q.Descripcion_TipoNota,
+                                 NomBeneficiario = q.NomBeneficiario,
+                                 IdCtaCble = q.IdCtaCble,
+                                 pc_Cuenta = q.pc_Cuenta,
+                                 dc_Valor = q.dc_Valor,
+                                 dc_Valor_Debe = q.dc_Valor_Debe,
+                                 dc_Valor_Haber = q.dc_Valor_Haber,
+                                 Nombre = q.Nombre,
+                                 Su_Descripcion = q.Su_Descripcion
+                             }).ToList();
+                }
+                return Lista;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
