211a2f6c0fa8cd6b85f6c82064540686c472a3c7
WendyCherres <wendycherres@gmail.com>
2020-02-27 09:39:13 -0500

    ...

---
+++ Academico/Core.Bus/CuentasPorCobrar/cxc_LiquidacionTarjetaDet_Bus.cs
@@ -0,0 +1,27 @@
+﻿using Core.Data.CuentasPorCobrar;
+using Core.Info.CuentasPorCobrar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Bus.CuentasPorCobrar
+{
+    public class cxc_LiquidacionTarjetaDet_Bus
+    {
+        cxc_LiquidacionTarjetaDet_Data odata = new cxc_LiquidacionTarjetaDet_Data();
+
+        public List<cxc_LiquidacionTarjetaDet_Info> GetList(int IdEmpresa, int IdSucursal, decimal IdLiquidacion)
+        {
+            try
+            {
+                return odata.GetList(IdEmpresa, IdSucursal, IdLiquidacion);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
