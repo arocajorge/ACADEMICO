7653c10377eee89552ecb98b240a9be1ed5bdcc6
arocajorge <arocajorge92@gmail.com>
2020-03-12 16:29:15 -0500

    Update ACA_007_Data.cs

--- Academico/Core.Data/Reportes/Academico/ACA_007_Data.cs
+++ Academico/Core.Data/Reportes/Academico/ACA_007_Data.cs
@@ -32,46 +32,52 @@ namespace Core.Data.Reportes.Academico
                 int IdParaleloIni = IdParalelo;
                 int IdParaleloFin = IdParalelo == 0 ? 9999999 : IdParalelo;
 
-                List<ACA_007_Info> Lista;
+                List<ACA_007_Info> Lista = new List<ACA_007_Info>();
                 using (EntitiesReportes Context = new EntitiesReportes())
                 {
-                    Lista = Context.VWACA_007.Where(q => q.IdEmpresa == IdEmpresa && 
-                    q.IdSede >= IdSedeIni && q.IdSede<=IdSedeFin &&
+                    var lst = Context.VWACA_007.Where(q => q.IdEmpresa == IdEmpresa &&
+                    q.IdSede >= IdSedeIni && q.IdSede <= IdSedeFin &&
                     q.IdAnio >= IdAnioIni && q.IdAnio <= IdAnioFin &&
                     q.IdNivel >= IdNivelIni && q.IdNivel <= IdNivelFin &&
                     q.IdJornada >= IdJornadaIni && q.IdJornada <= IdJornadaFin &&
                     q.IdCurso >= IdCursoIni && q.IdCurso <= IdCursoFin &&
                     q.IdParalelo >= IdParaleloIni && q.IdParalelo <= IdParaleloFin &&
-                    q.Fecha >= fecha_ini.Date && q.Fecha<=fecha_fin.Date).OrderBy(q=> new {q.OrdenNivel, q.OrdenJornada, q.OrdenCurso, q.OrdenParalelo }).Select(q => new ACA_007_Info
-                    {
-                        IdEmpresa = q.IdEmpresa,
-                        IdMatricula = q.IdMatricula,
-                        Cantidad = q.Cantidad,
-                        CodigoParalelo = q.CodigoParalelo,
-                        Fecha = q.Fecha,
-                        IdAnio= q.IdAnio,
-                        IdCurso=q.IdCurso,
-                        IdJornada=q.IdJornada,
-                        IdNivel=q.IdNivel,
-                        IdParalelo=q.IdParalelo,
-                        IdPlantilla=q.IdPlantilla,
-                        IdTipoPlantilla = q.IdTipoPlantilla,
-                        NomPlantillaTipo = q.NomPlantillaTipo,
-                        OrdenCurso=q.OrdenCurso,
-                        OrdenJornada=q.OrdenJornada,
-                        OrdenNivel=q.OrdenNivel,
-                        IdSede = q.IdSede,
-                        NomJornada=q.NomJornada,
-                        NomNivel=q.NomNivel,
-                        NomParalelo=q.NomParalelo,
-                        NomPlantilla=q.NomPlantilla,
-                        NomSede=q.NomSede,
-                        OrdenParalelo = q.OrdenParalelo,
-                        pe_sexo =q.pe_sexo,
-                        NomCurso = q.NomCurso,
-                        Descripcion=q.Descripcion
+                    q.Fecha >= fecha_ini.Date && q.Fecha <= fecha_fin.Date).ToList();
 
-                    }).ToList();
+                    foreach (var q in lst)
+                    {
+                        Lista.Add(new ACA_007_Info
+                        {
+                            IdEmpresa = q.IdEmpresa,
+                            IdMatricula = q.IdMatricula,
+                            Cantidad = q.Cantidad,
+                            CodigoParalelo = q.CodigoParalelo,
+                            Fecha = q.Fecha,
+                            IdAnio = q.IdAnio,
+                            IdCurso = q.IdCurso,
+                            IdJornada = q.IdJornada,
+                            IdNivel = q.IdNivel,
+                            IdParalelo = q.IdParalelo,
+                            IdPlantilla = q.IdPlantilla,
+                            IdTipoPlantilla = q.IdTipoPlantilla,
+                            NomPlantillaTipo = q.NomPlantillaTipo,
+                            OrdenCurso = q.OrdenCurso,
+                            OrdenJornada = q.OrdenJornada,
+                            OrdenNivel = q.OrdenNivel,
+                            IdSede = q.IdSede,
+                            NomJornada = q.NomJornada,
+                            NomNivel = q.NomNivel,
+                            NomParalelo = q.NomParalelo,
+                            NomPlantilla = q.NomPlantilla,
+                            NomSede = q.NomSede,
+                            OrdenParalelo = q.OrdenParalelo,
+                            pe_sexo = q.pe_sexo,
+                            NomCurso = q.NomCurso,
+                            Descripcion = q.Descripcion
+
+                        });
+                    }
+                    
                 }
                 return Lista;
             }
