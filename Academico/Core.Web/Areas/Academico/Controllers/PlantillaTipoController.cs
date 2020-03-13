38bbbffdb722ab685bb80fb7e39bfd47a2f756bc
EdgarSebastian01 <60885441+EdgarSebastian01@users.noreply.github.com>
2020-03-13 10:30:35 -0500

    ....

---
+++ Academico/Core.Web/Areas/Academico/Controllers/PlantillaTipoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Core.Info.Academico;
+using Core.Web.Helps;
+using Core.Bus.Academico;
+using Core.Bus.General;
+using Core.Info.Helps;
+using Core.Info.General;
+
+namespace Core.Web.Areas.Academico.Controllers
+{
+    public class PlantillaTipoController : Controller
+    {
+        #region Variables
+        aca_PlantillaTipo_Bus busPlantillaTipo = new aca_PlantillaTipo_Bus();
+        #endregion
+
+        // GET: Academico/PlantillaTipo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
