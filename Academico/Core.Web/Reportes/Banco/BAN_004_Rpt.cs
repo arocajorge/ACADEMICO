5b1f9638217f234141b7f60206f5763e40b7e9a3
WendyCherres <wendycherres@gmail.com>
2020-05-18 20:27:17 -0500

    ..

--- Academico/Core.Web/Reportes/Banco/BAN_004_Rpt.cs
+++ Academico/Core.Web/Reportes/Banco/BAN_004_Rpt.cs
@@ -6,6 +6,7 @@ using DevExpress.XtraReports.UI;
 using Core.Info.Reportes.Banco;
 using Core.Bus.Reportes.Banco;
 using System.Collections.Generic;
+using Core.Bus.General;
 
 namespace Core.Web.Reportes.Banco
 {
@@ -25,6 +26,17 @@ namespace Core.Web.Reportes.Banco
             lbl_usuario.Text = usuario;
             int IdEmpresa = p_IdEmpresa.Value == null ? 0 : Convert.ToInt32(p_IdEmpresa.Value);
             int IdArchivo = p_IdArchivo.Value == null ? 0 : Convert.ToInt32(p_IdArchivo.Value);
+
+            tb_empresa_Bus bus_empresa = new tb_empresa_Bus();
+            var emp = bus_empresa.get_info(IdEmpresa);
+            if (emp != null)
+            {
+                if (emp.em_logo != null)
+                {
+                    ImageConverter obj = new ImageConverter();
+                    lbl_imagen.Image = (Image)obj.ConvertFrom(emp.em_logo);
+                }
+            }
 
             BAN_004_Bus bus_rpt = new BAN_004_Bus();
             List<BAN_004_Info> lst_rpt = bus_rpt.get_list(IdEmpresa, IdArchivo);
