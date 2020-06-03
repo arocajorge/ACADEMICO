37a35b84521bc0465bde2c954f7550f8216c2fe6
WendyCherres <wendycherres@gmail.com>
2020-06-02 21:35:42 -0500

    ..

---
+++ Academico/Core.Info/Contabilidad/Contabilizacion/ct_ContabilizacionConciliacionNC_Info.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Info.Contabilidad.Contabilizacion
+{
+    public class ct_ContabilizacionConciliacionNC_Info
+    {
+        public int IdEmpresa { get; set; }
+        public decimal IdConciliacion { get; set; }
+        public int IdSucursal { get; set; }
+        public int IdBodega { get; set; }
+        public decimal IdNota { get; set; }
+        public decimal IdCobro { get; set; }
+        public System.DateTime Fecha { get; set; }
+        public double Valor { get; set; }
+        public string Observacion { get; set; }
+        public bool Estado { get; set; }
+        public string Referencia { get; set; }
+        public string pe_nombreCompleto { get; set; }
+        public Nullable<int> IdTipoCbte { get; set; }
+        public Nullable<decimal> IdCbteCble { get; set; }
+        public decimal IdAlumno { get; set; }
+        public string Codigo { get; set; }
+
+
+        public string IdString { get; set; }
+    }
+}
