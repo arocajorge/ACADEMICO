7ee444375f9d6efcbadbc82b536bfc88dc769442
arocajorge <arocajorge92@gmail.com>
2021-01-02 09:27:52 -0500

    qqqq

---
+++ Academico/Core.Admision/Controllers/AdmisionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Core.Admision.Controllers
+{
+    public class AdmisionController : Controller
+    {
+        // GET: Admision
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
